a0b6a7e43c0926dc44c978b5a6add6a730f30cf8
Hebert Montero Garcia <hmontero89@gmail.com>
2019-09-13 20:34:00 +0200

    Adds the 'O' to the game. Adds a mechanism to decide when the game has been won. Ignores clicks after a square has been clicked, or after the game has been won. Displays the game winner.

--- src/index.js
+++ src/index.js
@@ -14,15 +14,23 @@ function Square(props) {
 		constructor(props) {
 			super(props);
 			this.state = {
-				squares: Array(9).fill(null)
+				squares: Array(9).fill(null),
+				xIsNext: true
 			};
 		}
 
 		handleClick(i) {
 			const squares = this.state.squares.slice();
 
-			squares[i] = 'X';
-			this.setState({ squares });
+			if (calculateWinner(squares) || squares[i]) {
+				return;
+			}
+
+			squares[i] = this.state.xIsNext ? 'X' : 'O';
+			this.setState({
+				squares,
+				xIsNext: !this.state.xIsNext
+			});
 		}
 
     renderSquare(i) {
@@ -35,7 +43,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+			const winner = calculateWinner(this.state.squares);
+			let status;
+
+			if (winner) {
+				status = `Winner: ${winner}`;
+			} else {
+				status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+			}
   
       return (
         <div>
@@ -74,7 +89,27 @@ function Square(props) {
         </div>
       );
     }
-  }
+	}
+	
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+			}
+		}
+		return null;
+	}
   
   // ========================================
   
